5ba762686ecd29901476b030647692409f2da7a1
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-04-04 08:54:08 +0200

    added type Author

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Book } from '../../Models/Book';
+import { Author } from '../../Models/Author';
 import './Main.scss';
 
 function Main() {
@@ -9,7 +10,10 @@ function Main() {
     fetch('http://localhost:8000/books/')
       .then(response => response.json())
       .then(data => {
-        const res = data.data
+        const res = data.data.map((book: Book) => {
+          const authors = book.authors.map((author: Author) => '$(author.lastName');
+          return { ...book, authors };
+        });
         setBooks(res);
       })
       .catch(error => console.error(error));
