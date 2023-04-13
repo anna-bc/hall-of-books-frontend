1c3a8b24c942f7656c3146499f77b1b6c883346d
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-04-13 10:37:14 +0200

    added user redirection to main upon login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,21 +1,25 @@
-import { Dispatch, SetStateAction, useContext } from "react";
-
+import {  useContext, useEffect  } from "react";
 import LoginForm from "../components/LoginForm/LoginForm";
-import LoginSuccess from "../components/LoginForm/LoginSuccess";
-
-import { User } from "../models/User";
-
+import { useNavigate } from "react-router-dom"; 
 import { StateContext } from "../state/context/StateContext";
-
+import Main from "../components/Main/Main";
 
 function LoginPage() {
   const { state, dispatch } = useContext(StateContext);
+  const navigate = useNavigate(); 
+
+    useEffect(() => {
+    if (state.isAuthenticated) {
+      navigate("/");
+      }
+  }, [state.isAuthenticated, navigate]);
+
   return (
     <>
       {!state.isAuthenticated ? (
         <LoginForm state={state} dispatch={dispatch} />
       ) : (
-        <LoginSuccess username={state.userIdentifier} />
+        <Main />
       )}
     </>
   );
